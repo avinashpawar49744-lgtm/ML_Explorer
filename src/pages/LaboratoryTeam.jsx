7ba696a27aebcc455dcc8a0aba99289e5f990c68
Avinash Pawar <you@example.com>
2026-09-13 14:14:02 +0530

    Add HOD photo to laboratory team

--- src/pages/LaboratoryTeam.jsx
+++ src/pages/LaboratoryTeam.jsx
@@ -1,12 +1,13 @@
 import PageHeader from '../components/PageHeader';
 import ProfileCard from '../components/ProfileCard';
 import SocialLinks from '../components/SocialLinks';
+import hodsirPhoto from '../components/images/hodsir.png';
 
 export default function LaboratoryTeam() {
   return <>
     <PageHeader title="Laboratory Team" subtitle="Guidance, Coordination & Student Work" badge="AIML Department" />
     <section className="team-grid">
-      <ProfileCard name="Prof. Suraj Mahajan" designation="Head of Department (HOD)" photoLabel="Faculty Photo" imageSrc="/images/hod.jpg" />
+      <ProfileCard name="Prof. Suraj Mahajan" designation="Head of Department (HOD)" photoLabel="Faculty Photo" imageSrc={hodsirPhoto} />
       <ProfileCard name="Prof. Pranay Dongarwar" designation="Course Coordinator" photoLabel="Faculty Photo" imageSrc="/images/course-coordinator.jpg" />
       <ProfileCard name="Avinash Pawar" designation="Student" department="Artificial Intelligence & Machine Learning" photoLabel="Student Photo" imageSrc="/images/student.jpg" />
     </section>
